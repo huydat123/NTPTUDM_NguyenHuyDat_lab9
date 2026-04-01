1dd1e5351c55589b894c1a9c0d4dc4ec9fd8600c
huydat123 <huydat123.nt@gmail.com>
2026-04-01 09:39:13 +0700

    Delete Bai5 directory

--- Bai5/src/main/java/com/example/Bai5/repository/CategoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.Bai5.repository;
-
-import com.example.Bai5.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {}
--- Bai5/src/main/java/com/example/Bai5/repository/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.Bai5.repository;
-
-import com.example.Bai5.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {}
